705315cc3d25d75c56e72a7493d587e039319497
Julien Leray <leray.jul@gmail.com>
2020-05-19 17:24:42 +0200

    fix

--- secondaries.js
+++ secondaries.js
@@ -1,46 +1,46 @@
-var ctx = document.getElementById('chartAgeRep');
-var ctx2 = document.getElementById('chartSexRep');
-var myChart1 = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [14, 30, 56],
+// var ctx = document.getElementById('chartAgeRep');
+// var ctx2 = document.getElementById('chartSexRep');
+// var myChart1 = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             data: [14, 30, 56],
 
 
 
-            backgroundColor: ["#A2FFAE", "#6ECA7D", "#3A974E"]
-        }],
+//             backgroundColor: ["#A2FFAE", "#6ECA7D", "#3A974E"]
+//         }],
 
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
-    options: {
-        legend: false
-    }
-});
+//         // These labels appear in the legend and in the tooltips when hovering different arcs
+//         labels: [
+//             'Red',
+//             'Yellow',
+//             'Blue'
+//         ]
+//     },
+//     options: {
+//         legend: false
+//     }
+// });
 
 
-var myChart2 = new Chart(ctx2, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [64, 36],
-            backgroundColor: ["#22A7F0", "#F62459"]
-        }],
+// var myChart2 = new Chart(ctx2, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             data: [64, 36],
+//             backgroundColor: ["#22A7F0", "#F62459"]
+//         }],
 
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Homme',
-            'Femme'
-        ]
-    },
-    options: {
-        legend: false
-    }
-});
+//         // These labels appear in the legend and in the tooltips when hovering different arcs
+//         labels: [
+//             'Homme',
+//             'Femme'
+//         ]
+//     },
+//     options: {
+//         legend: false
+//     }
+// });
 
-console.log("myChart: ", myChart1)
+// console.log("myChart: ", myChart1)
